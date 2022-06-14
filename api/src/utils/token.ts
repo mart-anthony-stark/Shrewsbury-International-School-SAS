bd34e88137bdbd9f733ccb9b0cc0ca8ad083df43
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-14 20:06:11 +0800

    Fixed:token helpers

--- api/src/utils/token.ts
+++ api/src/utils/token.ts
@@ -2,9 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import { IGetUserAuthInfoRequest } from "../types/Request.types";
 const jwt = require("jsonwebtoken");
 
-export const createToken = (body: any) => {
-  return jwt.sign({ body }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+export const createToken = (
+  body: any,
+  secret: string,
+  expiresIn: string | number
+) => {
+  return jwt.sign({ body }, secret, {
+    expiresIn,
   });
 };
 
@@ -13,14 +17,14 @@ export const verifyToken = (
   res: Response,
   next: NextFunction
 ) => {
-  const authHeader = req.headers.token as string;
+  const authHeader = req.headers["access-token"] as string;
   if (!authHeader) return res.status(403).send({ error: "Unauthenticated" });
 
   const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.JWT_SECRET, (err: any, user: any) => {
+  jwt.verify(token, process.env.ACCESS_SECRET, (err: any, user: any) => {
     if (err) return res.status(403).send({ error: "Invalid Token" });
-
-    req.user = user.user;
+    console.log(user);
+    req.user = user.body;
     next();
   });
 };
