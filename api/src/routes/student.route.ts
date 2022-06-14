1f342563506695bb2dc1be81c95b54d5ad898e40
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-14 20:45:38 +0800

    Fixed: route paths

--- api/src/routes/student.route.ts
+++ api/src/routes/student.route.ts
@@ -1,11 +1,11 @@
-
-const router = require("express").Router();
+import { Router } from "express";
 const studentController = require("../controllers/student.controller");
+const router = Router();
 
-router.get("/student", studentController.getAll);
-router.get("/student/:id", studentController.getOne);
-router.post("/student", studentController.createOne);
-router.put("/student/:id", studentController.updateOne);
-router.delete("/student/:id", studentController.deleteOne);
+router.get("/", studentController.getAll);
+router.get("/:id", studentController.getOne);
+router.post("/", studentController.createOne);
+router.put("/:id", studentController.updateOne);
+router.delete("/:id", studentController.deleteOne);
 
-module.exports = router;
+export default router;
