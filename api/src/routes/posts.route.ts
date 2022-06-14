1b3f286a1c845d8d15180204fc8ff87e84ed1ed3
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-14 20:04:32 +0800

    remove

--- api/src/routes/posts.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import postController from "../controller/post.controller";
-const router = Router();
-
-router.get("/", postController.getAllPosts);
-
-export default router;
