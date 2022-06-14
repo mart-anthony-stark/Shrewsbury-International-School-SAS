6479777514bd2fd19d06d2dd20e6ef729b8be3e6
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-14 20:45:04 +0800

    Add: student controller

---
+++ api/src/controllers/student.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import Student from "../models/student.model";
+
+module.exports = {
+  // GET ALL DATA
+  getAll: async (req: Request, res: Response) => {
+    const students = await Student.find();
+    res.send(students);
+  },
+  // GET ONE DATA
+  getOne: async (req: Request, res: Response) => {
+    const student = await Student.findOne({ _id: req.params.id });
+    res.send(student);
+  },
+  // CREATE DATA
+  createOne: async (req: Request, res: Response) => {
+    const newStudent = new Student(req.body);
+    await newStudent.save();
+    res.send(newStudent);
+  },
+  // UPDATE DATA
+  updateOne: async (req: Request, res: Response) => {
+    const student = await Student.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.send(student);
+  },
+  // DELETE DATA
+  deleteOne: async (req: Request, res: Response) => {
+    const student = await Student.findByIdAndRemove(req.params.id);
+    res.send(student);
+  },
+};
