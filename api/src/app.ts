69cb75c2a797b79f3a5dc736be5d65b320f10d8b
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-14 20:44:49 +0800

    Add: student router

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import init from "./utils/init";
 import * as dotenv from "dotenv";
 import authRouter from "./routes/auth.route";
+import studentRouter from "./routes/student.route";
 dotenv.config({});
 
 const app: Application = express();
@@ -10,5 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/auth", authRouter);
+app.use("/student", studentRouter);
 
 init(app);
