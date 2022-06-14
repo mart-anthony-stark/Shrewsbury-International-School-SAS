0ae87ed7aae283b3f01727f560ad99bec85abac6
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-14 20:04:21 +0800

    remove

--- api/src/controller/post.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import Post from "../models/Post.model";
-
-export default {
-  getAllPosts: async (req: Request, res: Response) => {
-    const posts = await Post.find();
-    res.send(posts);
-  },
-};
