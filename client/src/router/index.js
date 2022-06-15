8ad7c5849eec9636cdc6f8b56f37b5d1ace851f2
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-15 19:40:47 +0800

    Setup frontend

---
+++ client/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/HelloWorld.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+export default router
