d71a81e2b863ff8cf59d0fb5f467f18836a38942
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-16 20:56:12 +0800

    Add: toast notif library

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+// import the styling for the toast
+import "mosha-vue-toastify/dist/style.css";
+// import global styles
 import "./index.css";
 import router from "./router/index";
 
