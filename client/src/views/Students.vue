241425c112ced077b13f17bc5573bd28f2f232f4
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-16 19:42:28 +0800

    Add: lazyload header

---
+++ client/src/views/Students.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { defineAsyncComponent } from "@vue/runtime-core";
+
+const Header = defineAsyncComponent(() => import("../components/Header.vue"));
+</script>
+
+<template>
+  <div>
+    <Header />
+    <h1>Students</h1>
+  </div>
+</template>
