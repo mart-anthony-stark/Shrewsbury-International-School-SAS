ed4bb3df22b8cc7c161ffc183433e9d89f53c58b
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-17 20:20:14 +0800

    Add: icons

--- client/src/views/Admins.vue
+++ client/src/views/Admins.vue
@@ -10,7 +10,7 @@ const Header = defineAsyncComponent(() => import("../components/Header.vue"));
     <main class="mx-2 sm:mx-6 md:mx-12">
       <h1 class="text-center text-3xl font-bold text-blue-500 my-4">Admins</h1>
       <button class="bg-green-500 text-white p-2 rounded hover:bg-green-600">
-        Add New Admin
+        <i class="fa fa-plus" aria-hidden="true"></i> Add New Admin
       </button>
       <div class="center hidden">
         <table>
@@ -26,8 +26,12 @@ const Header = defineAsyncComponent(() => import("../components/Header.vue"));
               <td>Salazar</td>
               <td>mart@gmail.com</td>
               <td class="grid grid-cols-2 gap-2">
-                <button class="edit-btn">Edit</button>
-                <button class="del-btn">Delete</button>
+                <button class="edit-btn">
+                  <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                </button>
+                <button class="del-btn">
+                  <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                </button>
               </td>
             </tr>
           </tbody>
@@ -38,5 +42,4 @@ const Header = defineAsyncComponent(() => import("../components/Header.vue"));
 </template>
 
 <style scoped>
-
 </style>
