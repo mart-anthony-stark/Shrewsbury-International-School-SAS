28717bb336c34729ef56f1af332e4bf84b95f59f
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-17 20:20:18 +0800

    Add: table

--- client/src/views/Students.vue
+++ client/src/views/Students.vue
@@ -7,6 +7,38 @@ const Header = defineAsyncComponent(() => import("../components/Header.vue"));
 <template>
   <div>
     <Header />
-    <h1>Students</h1>
+    <main class="mx-2 sm:mx-6 md:mx-12">
+      <h1 class="text-center text-3xl font-bold text-blue-500 my-4">
+        Students
+      </h1>
+      <button class="bg-green-500 text-white p-2 rounded hover:bg-green-600">
+        <i class="fa fa-plus" aria-hidden="true"></i> Add New Student
+      </button>
+      <div class="center hidden">
+        <table>
+          <thead>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>Action</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mart Anthony</td>
+              <td>Salazar</td>
+              <td>mart@gmail.com</td>
+              <td class="grid grid-cols-2 gap-2">
+                <button class="edit-btn">
+                  <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                </button>
+                <button class="del-btn">
+                  <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
   </div>
 </template>
