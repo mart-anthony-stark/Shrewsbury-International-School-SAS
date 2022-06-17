c3cea5bdf07ea9ff0cad42e33fd7c70f837893e6
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-17 20:13:24 +0800

    Add: table

--- client/src/views/Admins.vue
+++ client/src/views/Admins.vue
@@ -7,6 +7,36 @@ const Header = defineAsyncComponent(() => import("../components/Header.vue"));
 <template>
   <div>
     <Header />
-    <h1>Admins</h1>
+    <main class="mx-2 sm:mx-6 md:mx-12">
+      <h1 class="text-center text-3xl font-bold text-blue-500 my-4">Admins</h1>
+      <button class="bg-green-500 text-white p-2 rounded hover:bg-green-600">
+        Add New Admin
+      </button>
+      <div class="center hidden">
+        <table>
+          <thead>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>Action</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mart Anthony</td>
+              <td>Salazar</td>
+              <td>mart@gmail.com</td>
+              <td class="grid grid-cols-2 gap-2">
+                <button class="edit-btn">Edit</button>
+                <button class="del-btn">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
   </div>
 </template>
+
+<style scoped>
+
+</style>
