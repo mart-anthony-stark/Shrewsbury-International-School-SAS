78e41f37d07d317b48ce1618c29298bfca1d6fa2
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-19 21:01:50 +0800

    Add: insert and delete

--- client/src/views/Students.vue
+++ client/src/views/Students.vue
@@ -1,9 +1,29 @@
 <script setup>
-import { defineAsyncComponent, onBeforeMount, ref } from "@vue/runtime-core";
+import {
+  computed,
+  defineAsyncComponent,
+  onBeforeMount,
+  ref,
+} from "@vue/runtime-core";
+import { createToast } from "mosha-vue-toastify";
+import { useStore } from "vuex";
 
 const Header = defineAsyncComponent(() => import("../components/Header.vue"));
-
+const AddModal = defineAsyncComponent(() =>
+  import("../components/AddStudentModal.vue")
+);
+const DeleteModal = defineAsyncComponent(() =>
+  import("../components/DeleteModal.vue")
+);
+// const EditModal = defineAsyncComponent(() =>
+//   import("../components/EditStudentModal.vue")
+// );
+const store = useStore();
+const accessToken = computed(() => store.state.accessToken);
 const records = ref([]);
+const isAddmodalActive = ref(false);
+const delID = ref(null);
+const editObj = ref(null);
 
 const fetchRecords = async () => {
   const res = await fetch(`${import.meta.env.VITE_API_URL}/student`);
@@ -11,15 +31,56 @@ const fetchRecords = async () => {
   records.value = data;
 };
 
+const closeAddModal = () => {
+  isAddmodalActive.value = false;
+  fetchRecords();
+};
+
+const deleteRecord = async () => {
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/student/${delID.value}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "access-token": accessToken,
+      },
+    }
+  );
+  if (!res.ok) {
+    createToast("Something went wrong!", { type: "danger" });
+  } else {
+    createToast("Successfully deleted record", { type: "success" });
+    fetchRecords();
+  }
+  delID.value = null;
+};
+
 onBeforeMount(() => fetchRecords());
 </script>
 
 <template>
   <div>
+    <transition name="fade">
+      <add-modal @closeModal="closeAddModal()" v-if="isAddmodalActive" />
+    </transition>
+
+    <transition name="fade">
+      <delete-modal
+        @cancel="delID = null"
+        @confirm="deleteRecord()"
+        name="student"
+        v-if="delID !== null"
+      />
+    </transition>
+
     <Header />
     <main class="mx-2 sm:mx-6 md:mx-12">
       <h1 class="text-center text-3xl font-bold text-red-700 my-4">Students</h1>
-      <button class="bg-green-500 text-white p-2 rounded hover:bg-green-600">
+      <button
+        @click="isAddmodalActive = true"
+        class="bg-green-500 text-white p-2 rounded hover:bg-green-600"
+      >
         <i class="fa fa-plus" aria-hidden="true"></i> Add New Student
       </button>
       <div class="center hidden">
@@ -37,14 +98,13 @@ onBeforeMount(() => fetchRecords());
               <td>{{ student.firstname }}</td>
               <td>{{ student.middlename }}</td>
               <td>{{ student.lastname }}</td>
-              <td>{{ student.email }}</td>
               <td>{{ student.course }}</td>
               <td>{{ student.section }}</td>
               <td class="grid grid-cols-2 gap-2">
                 <button class="edit-btn">
                   <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
                 </button>
-                <button class="del-btn">
+                <button @click="delID = student._id" class="del-btn">
                   <i class="fa fa-trash" aria-hidden="true"></i> Delete
                 </button>
               </td>
