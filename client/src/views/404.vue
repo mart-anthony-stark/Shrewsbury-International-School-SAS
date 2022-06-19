05e315c249070a5b6c1ffd0871a3289014106db9
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-19 19:38:15 +0800

    Add: 404 route view

---
+++ client/src/views/404.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="center h-screen flex flex-col gap-2">
+    <h1 class="text-gray-700 text-2xl">404 | Page Not Found</h1>
+    <router-link to="/">
+      <h3 class="text-red-800 text-lg underline">Back To Home</h3>
+    </router-link>
+  </div>
+</template>
