9f6e90ab1949538560102ccbd525367e215cb507
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-21 20:28:22 +0800

    Add: util

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,10 +1,16 @@
-const handleDownload = ({ title, selector }) => {
+/**
+ *  Downloads dom element as png
+ * @param {object} title | output filename, querySelector
+ *
+ */
+export const handleDownload = ({ title, selector }) => {
   const svg = document.querySelector(selector);
   const svgData = new XMLSerializer().serializeToString(svg);
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
   const img = new Image();
-  img.onload = () => {
+  //   img.onload = () => {
+  setTimeout(() => {
     canvas.width = img.width;
     canvas.height = img.height;
     ctx?.drawImage(img, 0, 0);
@@ -13,8 +19,8 @@ const handleDownload = ({ title, selector }) => {
     downloadLink.download = title || "QR Code";
     downloadLink.href = `${pngFile}`;
     downloadLink.click();
-  };
+  }, 1000);
+  //   };
   img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+  console.log("download");
 };
-
-module.exports = { handleDownload };
