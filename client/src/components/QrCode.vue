63bc33f3f7e202c2551ee0f4c404f2c5101e4768
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-21 19:45:38 +0800

    Add: qrcode details

--- client/src/components/QrCode.vue
+++ client/src/components/QrCode.vue
@@ -1,19 +1,41 @@
 <script setup>
+import QrcodeVue from "qrcode.vue";
+
 const { value } = defineProps(["value"]);
 </script>
 
 <template>
   <div class="container">
-    <div class="qr-container"></div>
+    <div @click="$emit('onClose')" class="overlay"></div>
+    <div class="qr-container">
+      <qrcode-vue :value="value._id" :size="280"></qrcode-vue>
+      <h2 class="text-center font-bold text-red-800">
+        {{ value.lastname }}, {{ value.firstname }}
+        {{ value.middlename[0].toUpperCase() }}.
+      </h2>
+      <h2 class="text-center font-bold text-red-800">
+        {{ value.course }} - {{ value.section }}
+      </h2>
+      <img
+        id="logo"
+        src="../assets/school.png"
+        alt="Shrewsbury International School"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.container {
+.overlay {
   @apply fixed inset-0 z-10;
   background: rgba(0, 0, 0, 0.4);
 }
 .qr-container {
-  @apply fixed bg-white rounded p-2 top-1/2 left-1/2;
+  @apply fixed bg-white rounded p-4 top-1/2 left-1/2 z-30;
+  transform: translate(-50%, -50%);
+}
+#logo {
+  height: 50px;
+  margin: auto;
 }
 </style>
