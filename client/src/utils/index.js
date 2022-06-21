cc5dffe9075ce0d246a432888b38f66318f14d4c
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-21 19:50:15 +0800

    Add: download util method

---
+++ client/src/utils/index.js
@@ -0,0 +1,20 @@
+const handleDownload = ({ title, selector }) => {
+  const svg = document.querySelector(selector);
+  const svgData = new XMLSerializer().serializeToString(svg);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx?.drawImage(img, 0, 0);
+    const pngFile = canvas.toDataURL("image/png");
+    const downloadLink = document.createElement("a");
+    downloadLink.download = title || "QR Code";
+    downloadLink.href = `${pngFile}`;
+    downloadLink.click();
+  };
+  img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+};
+
+module.exports = { handleDownload };
