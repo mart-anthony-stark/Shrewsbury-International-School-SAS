e7f5bf31d6ff45a92549456314fbfe3a07c4707b
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-21 19:28:46 +0800

    Add: qrcode component

---
+++ client/src/components/QrCode.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { value } = defineProps(["value"]);
+</script>
+
+<template>
+  <div class="container">
+    <div class="qr-container"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  @apply fixed inset-0 z-10;
+  background: rgba(0, 0, 0, 0.4);
+}
+.qr-container {
+  @apply fixed bg-white rounded p-2 top-1/2 left-1/2;
+}
+</style>
